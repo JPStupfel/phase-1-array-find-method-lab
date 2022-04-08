3d69b0c5f7331c7e6382adc5f1c7259b61294e79
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-08 11:34:06 -0500

    commit tree e8a47589f5ba56fc3b6cbf3c702600565ba1902e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+const winners = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+const loser = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+
+  const superbowlWin = function(array){
+
+    return array.find(element => element.result === "W") ? array.find(element => element.result === "W").year : undefined
+    
+    // winningObject = array.find(element => element.result === "W")
+
+
+    // yearOfWin = winningObject.year
+    // console.log(winningObject);
+    // console.log(yearOfWin);
+
+  }
